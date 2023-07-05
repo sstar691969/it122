50fab3b1765090cc71882d28919ef24d82820487
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-07-05 15:24:06 -0700

    2nd commit package.json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ http.createServer((req,res) => {
             res.writeHead(200, {'Content-Type': 'text/plain'});
             //res.end('Willy Home page');
             break;
-        case '/about':
+        case '/abouts':
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('Not found');
             break;
