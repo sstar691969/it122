aa6c305f2296960f80cdeee1c049c3923c9c2ed4
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-07-06 23:29:41 -0700

    1st commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
 
-const http = require("http")
+const http = require("http");
 http.createServer((req,res) => {
     var path = req.url.toLowerCase();    
     switch(path) {
         case '/':
             res.writeHead(200, {'Content-Type': 'text/plain'});
-            //res.end('Willy Home page');
+            res.end(' Home page');
             break;
         case '/about':
             res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('about this page');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Not found');
             break;
-        //default:
-            //res.writeHead(404, {'Content-Type': 'text/plain'});
-            //res.end('Not found');
-            //break;
     }    
 }).listen(process.env.PORT || 3000);
