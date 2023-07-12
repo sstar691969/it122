f91d5ce62c9222906d083a1019009f67afa7a9b7
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-07-12 16:59:03 -0700

    res.render issue

---
+++ week2Assignment/data.js
@@ -0,0 +1,26 @@
+//import { data} from "./data";
+// atleast 4 attribut.es also, a key attribute plus 5 objects representing the items.
+const car_volkwagen = [ 
+    {id : 1, model :"Bug", year : 2023, engine: 'v6', power : 'windows', ignition : 'keyless'},
+    {id : 2, model : "Jetta", year : 2023, engine : 'v8', power : 'windows', ignition : 'keyless' },
+    {id : 3, model : "Luxury cc", year : 2023, engine : 'v8', power : 'windows', ignition : 'keyless' },
+    {id : 4, model : "ID.4", year : 2023, engine : 'v8', power : 'windows', ignition : 'keyless' },
+    {id : 5, model : "Atlas", year : 2023, engine : 'v8', power : 'windows', ignition : 'keyless' },
+    {id : 6, model : "Iguan", year : 2023, engine : 'v8', power : 'windows', ignition : 'keyless' }
+    ];
+
+    //cars.reverse();
+    //console.log(cars);
+
+// add getAll method which returns all array data and getItem method which return a single array item according to the requested key
+//let students = [
+    export const getAll = () => {
+        return car_volkwagen
+
+        
+
+
+    } 
+
+
+    // your module should export the getAll and getItem methods to use in other modules
