6d6ed45ee3af33af6fae630adca52e240307c29b
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-07-26 18:43:20 -0700

    1st and final intergrate mongodb:index.js final commit and views/detail/home

--- week2Assignment/index.js
+++ week2Assignment/index.js
@@ -1,8 +1,9 @@
 
 //import your data.js module into index.js
 import express from 'express';
-import { getAll } from "./data.js";
+
 import { getItem } from "./data.js";
+import { Cars } from "../models/Cars.js";
 
 //install the express and ejs node modules
 //convert your index.js module to use Express app syntax
@@ -11,14 +12,30 @@ let app = express();
 app.set("view engine","ejs");
 app.set('port', process.env.PORT || 3000);
 
+// Static files
+
+app.use(express.static('public'))
+//app.use('/css', express.static(dirname + 'public/css'))
+//app.use('/js', express.static(__dirname + 'public/js'))
+//app.use('/img', express.static(__dirname + 'public/img'))
+
+
+
+
+
 
 
 
     app.get("/",(req,res) => {
         //res.status(200).json, res.render("home")(data.GetAllItems);
         // send plain text response
-
-    res.render("home",{car_volkwagen:getAll()} );
+        Cars.find({}).lean()
+  .then((cars) => {
+    
+    res.render("home",{car_volkwagen:cars})
+  })
+  .catch(err => console.log(err));
+    
     //res.send(getAll());
 });
     
@@ -33,16 +50,19 @@ app.set('port', process.env.PORT || 3000);
 
 // note: create new variable let vehicle and create new variable getAll
 app.get("/detail/:id",(req,res) => {
-    //let vehicle = getItem(req.params['id']);
-    //if (vehicle) {
-       //res.render(`Info for vehicle: ${vehicle.model}`);
-       //res.render("detail",vehicle);
-    //}
+      Cars.findOne({id:req.params.id}).lean()
+        .then((cars) => {
+            //res.render('detail', {cars} );
 
-    //res.render("detail",{vehicle:vehicle});
-    res.render("detail",{vehicle:getItem(req.params.id)});
-   //res.render(`Info for volkwagen: $(req.params.id)`);
+            res.render("detail",{vehicle:cars});
+        })
+        .catch(err => next(err));
+    
+    
+   
     });
+
+    //.catch(err => next(err));
     
     
 
